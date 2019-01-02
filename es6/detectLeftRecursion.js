4cb4c59c9136b3e7a94908c01e4a59766d4fe43b
James Smith <james.smith@openmathematics.org>
2019-01-02 16:47:41 +0000

    Removed removing cycles when detecting left recursion.

--- es6/detectLeftRecursion.js
+++ es6/detectLeftRecursion.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const eliminateCycles = require('./eliminateCycles'),
-      detectImplicitLeftRecursion = require('./detectImplicitLeftRecursion');
+const detectImplicitLeftRecursion = require('./detectImplicitLeftRecursion');
 
 function detectLeftRecursion(rules) {
-  rules = eliminateCycles(rules);
-
   const leftRecursiveRuleName = detectImplicitLeftRecursion(rules);
 
   return leftRecursiveRuleName;
