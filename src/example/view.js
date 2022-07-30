94dd402b46dc9ef9c074201a50f71eb1a6563033
James Smith <james.smith@djalbat.com>
2022-07-30 17:38:40 +0100

    Rebuilt and re-published.

--- src/example/view.js
+++ src/example/view.js
@@ -143,26 +143,382 @@ class View extends Element {
   }
 
   static initialBNF = ` 
-    A ::= "d" 
-    
-        | B 
-    
-        | "e"
-    
-        ;
-    
-    B ::= "b"
-    
-        | A
-    
-        | "c"
+document                             ::=   ( topLevelInstruction | verticalSpace | error )+ ;
 
-        ;
+
+
+topLevelInstruction                  ::=   rule 
+
+                                       |   axiom 
+
+                                       |   lemma 
+
+                                       |   theorem 
+
+                                       |   conjecture 
+
+                                       |   metalemma 
+
+                                       |   metatheorem 
+
+                                       |   metaconjecture
+
+                                       |   typeDeclaration 
+                                           
+                                       |   variableDeclaration 
+                                           
+                                       |   comparatorDeclaration 
+                                           
+                                       |   combinatorDeclaration 
+                                           
+                                       |   constructorDeclaration 
+                                           
+                                       |   disjointTypeDeclaration 
+                                           
+                                       |   metavariableDeclaration 
+                                           
+                                       |   dependentTypeDeclaration 
+                                           
+                                       |   typesDeclaration 
+                                           
+                                       |   variablesDeclaration 
+                                           
+                                       |   comparatorsDeclaration 
+                                           
+                                       |   combinatorsDeclaration 
+                                           
+                                       |   constructorsDeclaration 
+                                           
+                                       |   disjointTypesDeclaration 
+                                           
+                                       |   metavariablesDeclaration 
+                                           
+                                       |   dependentTypesDeclaration 
+                                           
+                                       ;
+
+
+
+verticalSpace                        ::=   <END_OF_LINE>+ ;
+
+
+
+error                                ::=   . ;
+
+
+
+rule                                 ::=   "Rule" "(" label ( "," label )* ")" <END_OF_LINE> ( premise | premises )? conclusion metaproof? ;
+
+axiom                                ::=   "Axiom" "(" label ( "," label )* ")" <END_OF_LINE> ( indicativeConditional | unqualifiedStatement ) ; 
+
+lemma                                ::=   "Lemma" "(" label ( "," label )* ")"? <END_OF_LINE> ( indicativeConditional | unqualifiedStatement ) proof ;
+
+theorem                              ::=   "Theorem" "(" label ( "," label )* ")" <END_OF_LINE> ( indicativeConditional | unqualifiedStatement ) proof ;
+
+conjecture                           ::=   "Conjecture" "(" label ( "," label )* ")" <END_OF_LINE> ( indicativeConditional | unqualifiedStatement ) proof? ;
+
+metalemma                            ::=   "Metalemma" "(" label ( "," label )* ")" <END_OF_LINE> ( metaIndicativeConditional | unqualifiedMetastatement ) metaproof ;
+
+metatheorem                          ::=   "Metatheorem" "(" label ( "," label )* ")" <END_OF_LINE> ( metaIndicativeConditional | unqualifiedMetastatement ) metaproof ;
+
+metaconjecture                       ::=   "Metaconjecture" "(" label ( "," label )* ")" <END_OF_LINE> ( metaIndicativeConditional | unqualifiedMetastatement ) ;
+
+typeDeclaration                      ::=   "Type" type ( ":" type )? <END_OF_LINE> ;
+ 
+variableDeclaration                  ::=   "Variable" variable ":" type <END_OF_LINE> ;
+ 
+comparatorDeclaration                ::=   "Comparator" statement <END_OF_LINE> ;
+ 
+combinatorDeclaration                ::=   "Combinator" expression ( ":" type )? <END_OF_LINE> ;
+ 
+constructorDeclaration               ::=   "Constructor" term ( ":" type )? <END_OF_LINE> ;
+ 
+disjointTypeDeclaration              ::=   "DisjointType" disjointType ":" type ( "," type )+ <END_OF_LINE> ;
+                                       
+metavariableDeclaration              ::=   "Metavariable" metavariable ":" ( "Statement" | "Context" ) <END_OF_LINE> ;
+ 
+dependentTypeDeclaration             ::=   "DependentType" dependentType ":" type <END_OF_LINE> ;
+                                       
+typesDeclaration                     ::=   "Types" type ( ":"  type )? <END_OF_LINE> ;
+
+variablesDeclaration                 ::=   "Variables" variable ( "," variable )+ ":" type <END_OF_LINE> ;
+ 
+comparatorsDeclaration               ::=   "Comparators" statement ( "," statement )+ <END_OF_LINE> ;
+ 
+combinatorsDeclaration               ::=   "Combinators" expression ( "," expression )+ ( ":" type )? <END_OF_LINE> ;
+ 
+constructorsDeclaration              ::=   "Constructors" term ( "," term )+ ( ":" type )? <END_OF_LINE> ;
+ 
+disjointTypesDeclaration             ::=   "DisjointTypes" disjointType ( "," disjointType )+ ":" type ( "," type )+ <END_OF_LINE> ;
+ 
+metavariablesDeclaration             ::=   "Metavariables" metavariable ( "," metavariable )+ ":" ( "Statement" | "Context" ) <END_OF_LINE> ;
+ 
+dependentTypesDeclaration            ::=   "DependentTypes" dependentType ( "," dependentType )+ ":" type <END_OF_LINE> ;
+  
+
+  
+premise                              ::=   "Premise" <END_OF_LINE> unqualifiedMetastatement ;
+
+premises                             ::=   "Premises" <END_OF_LINE> unqualifiedMetastatement unqualifiedMetastatement+ ;
+
+conclusion                           ::=   "Conclusion" <END_OF_LINE> unqualifiedMetastatement ;
+
+
+
+metaproof                            ::=   "Proof" <END_OF_LINE> 
+
+                                           metastatementDefinition*
+
+                                           metaProofDerivation? 
+                                          
+                                           qualifiedMetastatement ;
+                                          
+                                          
+
+metaProofDerivation                  ::=   ( metaSublemma | qualifiedMetastatement | qualifiedStatement )+  
+
+                                           "Therefore" <END_OF_LINE> ;                                           
+
+metaIndicativeConditional            ::=   "Suppose" <END_OF_LINE> unqualifiedMetastatement+ 
+
+                                           "Hence" <END_OF_LINE> qualifiedMetastatement ;
+
+metaSublemma                         ::=   "Suppose" <END_OF_LINE> 
+
+                                           ( metaSublemma | qualifiedMetastatement | qualifiedStatement )+ 
+
+                                           ( 
+                                          
+                                             "Then" <END_OF_LINE> 
+                                            
+                                             ( metaSublemma | qualifiedMetastatement | qualifiedStatement )+ 
+                                          
+                                           )? 
+                                          
+                                           "Hence" <END_OF_LINE> qualifiedMetastatement ;
+
+
+
+proof                                ::=   "Proof" <END_OF_LINE> 
+
+                                           statementDefinition*
+
+                                           proofDerivation? 
+                                          
+                                           qualifiedStatement ;
+                                                                                         
+                                                                                         
+                                                                                         
+proofDerivation                      ::=   ( sublemma | qualifiedStatement )+ 
+
+                                           "Therefore" <END_OF_LINE> ;
+
+indicativeConditional                ::=   "Suppose" <END_OF_LINE> unqualifiedStatement+ 
+
+                                           "Hence" <END_OF_LINE> qualifiedStatement ;
+
+sublemma                             ::=   "Suppose" <END_OF_LINE> 
+
+                                           ( subLemma | qualifiedStatement )+ 
+
+                                           ( 
+                                          
+                                             "Then" <END_OF_LINE> 
+                                            
+                                             ( subLemma | qualifiedStatement )+ 
+                                          
+                                           )? 
+                                          
+                                           "Hence" <END_OF_LINE> qualifiedStatement ;
+
+
+
+metastatementDefinition              ::=   "Let" unqualifiedMetastatement ;                                           
+                                          
+statementDefinition                  ::=   "Let" unqualifiedStatement ;                                           
+
+
+
+unqualifiedMetastatement!            ::=   metastatement <END_OF_LINE> 
+
+                                       |   nonsense... <END_OF_LINE> 
+                                       
+                                       ;
+
+qualifiedMetastatement!              ::=   metastatement qualification? <END_OF_LINE> 
+
+                                       |   nonsense... qualification? <END_OF_LINE> 
+                                        
+                                       ;
+
+unqualifiedStatement!                ::=   statement <END_OF_LINE>
+
+                                       |   nonsense... <END_OF_LINE> 
+                                       
+                                       ;
+
+qualifiedStatement!                  ::=   statement qualification? <END_OF_LINE> 
+
+                                       |   nonsense... qualification? <END_OF_LINE> 
+                                       
+                                       ;
+
+
+
+nonsense                             ::=   ( "by" | "from" | [name] | [custom] | [special] | [reserved] | [unassigned] )+ ;
+
+
+
+argument                             ::=   type | expression ;
+
+
+
+qualification                        ::=   ( "by" | "from" ) reference ;
+
+
+
+dependentType                        ::=   [name]<NO_WHITESPACE>"(" term ")" ;
+
+
+
+metavariable                         ::=   [name] ( <NO_WHITESPACE>"(" term ")" )? ;
+
+reference                            ::=   [name] ( <NO_WHITESPACE>"(" term ")" )? ;
+
+context                              ::=   [name] ( <NO_WHITESPACE>"(" term ")" )? ;
+
+label                                ::=   [name] ( <NO_WHITESPACE>"(" term ")" )? ;
+
+
+
+disjointType                         ::=   [name] ;
+
+variable                             ::=   . ; 
+
+type                 ::= "Number"
+
+                       | "RealNumber"
+
+                       | "RationalNumber"
+
+                       | "Integer"
+
+                       | "NaturalNumber"
+
+                       | "NonZeroInteger"
+
+                       | _
+
+                       ;
+
+term!                ::= arithmeticTerm
+
+                       | _
+
+                       ;
+
+expression!          ::= arithmeticExpression
+
+                       | _
+
+                       ;
+
+statement!           ::= "(" statement ")"
+
+                       | "¬" <NO_WHITESPACE> statement
+
+                       | statement ( "/\\\\" | "\\\\/" | "iff" | "=>" | "<=>" ) statement
+
+                       | arithmeticAtatement
+
+                       | typeAssertion
+
+                       | equality
+
+                       ;
+
+typeAssertion        ::= expression ":" type ;
+
+equality             ::= expression "=" expression ;
+
+metastatement!       ::= [name] "=" naturalNumber
+
+                       | "(" metastatement ")"
+
+                       | "¬" <NO_WHITESPACE> metastatement
+
+                       | metastatement ( "/\\\\" | "\\\\/" | "iff" | "=>" | "<=>" ) metastatement
+
+                       | contextDefinition
+
+                       | proofAssertion
+
+                       | metavariable
+
+                       | subproof
+
+                       ;
+
+contextDefinition    ::= context "=" ( judgement | context ) ( "," ( judgement | context ) )* ;
+
+proofAssertion       ::= context "|-" judgement ;
+
+judgement            ::= reference "::" metastatement ;
+
+subproof             ::= "[" metastatement "]" "..." metastatement ;
+
+arithmeticTerm       ::= rationalNumber
+
+                       | integer
+
+                       | naturalNumber
+
+                       | variable
+
+                       ;
+
+arithmeticExpression ::= "(" argument ")"
+
+                       | argument ( "+" | "-" | "×" | "÷" ) argument
+
+                       | arithmeticTerm
+
+                       ;
+
+arithmeticAtatement  ::= argument ( "<" | "<=" | ">=" | ">" ) argument ;
+
+naturalNumber        ::= "successor" <NO_WHITESPACE> "(" argument ")"
+
+                       | "predecessor" <NO_WHITESPACE> "(" argument ")"
+
+                       | "zero"
+
+                       | variable
+
+                       ;
+
+integer              ::= "successor" <NO_WHITESPACE> "(" argument ")"
+
+                       | "predecessor" <NO_WHITESPACE> "(" argument ")"
+
+                       | "-" <NO_WHITESPACE> argument
+
+                       | naturalNumber
+
+                       | variable
+
+                       ;
+
+rationalNumber       ::= argument <NO_WHITESPACE> "/" <NO_WHITESPACE> argument
+
+                       | variable
+
+                       ;
 `;
 
-  static initialContent = "gefh";
+  static initialContent = "n/n";
 
-  static initialStartRuleName = "";
+  static initialStartRuleName = "term";
 
   static initialLexicalPattern = ".";
 
