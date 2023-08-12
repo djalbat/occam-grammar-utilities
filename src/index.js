5b58b9dc12b66b2a00aa9197333bf4b5123d0ccb
James Smith <james.smith@djalbat.com>
2023-08-12 16:53:46 +0100

    Added a polyfill for the findLastIndex() array method.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 "use strict";
 
+if (!Array.prototype.findLastIndex) {
+  Array.prototype.findLastIndex = function (callback, thisArg) {
+    for (let i = this.length - 1; i >= 0; i--) {
+      if (callback.call(thisArg, this[i], i, this)) return i;
+    }
+    return -1;
+  };
+}
+
 export { default as eliminateLeftRecursion } from "./eliminateLeftRecursion";
 
 export { default as ExampleLexer } from "./lexer/example";
